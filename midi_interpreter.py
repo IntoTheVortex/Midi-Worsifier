c4a7a279ecfb318cbf24d428e7272dc0921c81a8
IntoTheVortex <amberadelaideshore@gmail.com>
2022-05-10 10:11:24 -0700

    extra pip comm for midi

--- midi_interpreter.py
+++ midi_interpreter.py
@@ -1,12 +1,19 @@
 import sys
+import midi
 
 
 
+#info:
+'''https://stackoverflow.com/questions/50158890/attributeerror-module-midi-has-no-attribute-read-midifile
+this link was needed to get the midi to work after pip install midi
+'''
 
 
 
+#https://www.personal.kent.edu/~sbirch/Music_Production/MP-II/MIDI/midi_file_format.htm
 def read_midi(midi_file):
-    pass
+    pattern = midi.read_midifile(midi_file)
+    print(pattern)
 
 
 
