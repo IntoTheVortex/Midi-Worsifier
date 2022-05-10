a4394d0dac0cd48f14c14829a93816f11efa544b
IntoTheVortex <amberadelaideshore@gmail.com>
2022-05-10 16:14:30 -0700

    ready to add wave generation

--- midi_interpreter.py
+++ midi_interpreter.py
@@ -1,58 +1,66 @@
 import sys
 import numpy as np
-from mido import MidiFile, Message
+from mido import MidiFile, Message, tempo2bpm, tick2second
+import sine_by_freq
 
 #https://soundprogramming.net/file-formats/midi-note-frequencies/
 NOTES = 'notes.txt'
 
 
 #info:
-'''https://stackoverflow.com/questions/50158890/attributeerror-module-midi-has-no-attribute-read-midifile
-this link was needed to get the midi to work after pip install midi
-change to this:
+'''
+changed library to this:
 https://mido.readthedocs.io/en/latest/installing.html
 '''
 
+#development notes:
+    #ignoring tempo & velocity for now
 
 
-#https://www.personal.kent.edu/~sbirch/Music_Production/MP-II/MIDI/midi_file_format.htm
 def read_midi(midi_file):
-    #for python-midi:
-    #pattern = midi.read_midifile(midi_file)
-    #print(pattern)
-
     #ref https://mido.readthedocs.io/en/latest/midi_files.html
     #for mido:
     mid = MidiFile(midi_file)
+    midi_length = mid.length
+    print(midi_length)
 
-    #all_notes = np.zeros((len(mid.tracks), 50))
-    #print(len(mid.tracks))
-    #print(all_notes.shape)
+    #tempo:
+    tempo = 500000 #default tempo from mido 
 
     all_notes = []
     input_notes = []
+    deltas = []
     for i, track in enumerate(mid.tracks):
         print('Track {}: {}'.format(i, track.name))
         input_notes = input_notes.clear
         input_notes = []
         for msg in track:
-            #print(msg)
             if msg.type == 'note_on':
                 #print(msg.note)
-                input_notes.append(msg.note)
+                #finally an answer: https://stackoverflow.com/questions/45772214/convert-time-tick-in-python-midi-mido-read-save-file
+                if msg.time > 0:
+                    #round to only 4 decimal places
+                    delta = round(tick2second(msg.time, mid.ticks_per_beat, tempo), 4)
+                else:
+                    delta = 0
+                deltas.append(delta)
+                input_notes.append((msg.note, delta))
+            elif msg.type == 'set_tempo':
+                tempo = msg.tempo
+            elif msg.type == 'time_signature':
+                print(msg)
         if(input_notes):
-            print(len(input_notes))
             all_notes.append(input_notes)
 
-        
-    print(all_notes)
+    #print(all_notes)
+    ave_delta = sum(deltas)/len(deltas)
+    return all_notes, ave_delta
     
 class Midi_chart:
     def __init__(self):
         self.setup_notes()
 
-        #for i in range(len(self.notes)):
-            #print(i, self.notes[i])
+
 
     def setup_notes(self):
         notes = []
@@ -62,7 +70,30 @@ class Midi_chart:
         self.notes = [float(x) for x in notes]
         self.notes = [round(x) for x in self.notes]
 
+    def display(self):
+        for i in range(len(self.notes)):
+            print(i, self.notes[i])
 
+
+
+def decode_midi(input, chart, delta):
+    n_tracks = len(input)
+    for i in range(n_tracks):
+        #convert each note to freq, and time in seconds
+        for j in range(len(input[i])-1, 0, -1):
+            note_tuple = input[i][j] 
+            note = chart.notes[note_tuple[0]] #chart at index of note num is frequency
+            delta_time = note_tuple[1]
+            #time = 0 # ... didn't get note_off events (none in sample?)
+
+            #cases: last note, not last but time=0
+            if j+1 >= len(input[i]):
+                time = delta
+            else:
+                time = input[i][j+1][1] #the delta from the following note
+
+            #TODO
+            #write_sine(note, time) #this call when sine_by_freq updated
 
 
 
@@ -72,8 +103,9 @@ def main():
         sys.exit(1)
     midi_file = sys.argv[1]
     print(midi_file)
-    read_midi(midi_file)
+    input_notes, delta = read_midi(midi_file)
     chart = Midi_chart()
+    note_instructions = decode_midi(input_notes, chart, delta)
 
 
 
