684acd49f023a7eb85d07a453fd555205f0f8274
IntoTheVortex <amberadelaideshore@gmail.com>
2022-06-04 12:12:46 -0700

    fixed the rest issue

--- midi_interpreter.py
+++ midi_interpreter.py
@@ -14,26 +14,24 @@ changed library to this:
 https://mido.readthedocs.io/en/latest/installing.html
 '''
 
-## TODO
-    # interpret note_on with time=0 as note_off
-    # problem is don't have a way to parse rests for files 
-    #  that don't have the note_off messages
-
-
 
 
 def read(midi_file):
     mid = MidiFile(midi_file)
+    has_off = False
 
     #check to see if midi file contains note_off events
-    #for i, track in enumerate(mid.tracks):
-    if False:
+    for _, track in enumerate(mid.tracks):
         for msg in track:
+            print(msg)
             if msg.type == 'note_off':
-                return read_midi_note_off(midi_file), True
+                has_off = True
+                break
 
+    if has_off:
+        return read_midi_note_off(midi_file)
     #else no note_off messages in midi:
-    return read_midi(midi_file), False
+    return read_midi(midi_file)
 
 
 def read_midi_note_off(midi_file):
@@ -72,28 +70,46 @@ def read_midi(midi_file):
     #midi_length = mid.length
 
     tempo = 500000 #default tempo from mido 
+    note_playing = False
 
     all_notes = []
     input_notes = []
-    deltas = []
+    last_time = 0
+    last_note = 0
     for i, track in enumerate(mid.tracks):
         print('Track {}: {}'.format(i, track.name))
         input_notes = input_notes.clear
         input_notes = []
         for msg in track:
             print(msg) #TODO remove
-            #if msg.type == 'note_on' and msg.velocity == 0:
-            if (msg.type == 'note_on'  and msg.velocity == 0) or msg.type == 'note_off':
-                if msg.time != 0:
-                    #delta = tick2second(msg.time, mid.ticks_per_beat, tempo)
-                    delta = tick2second(msg.time, 110, tempo)
-                    print("ticks", mid.ticks_per_beat)
+
+            #actual note_on:
+            if (msg.type == 'note_on'  and msg.velocity != 0):
+                if note_playing:
+                    #record the note that was playing and it's duration
+                    delta = tick2second(msg.time, mid.ticks_per_beat, tempo)
+                    input_notes.append((last_note, delta))
+                    last_time = 0
+                else:
+                    #record the rest
+                    delta = tick2second(msg.time, mid.ticks_per_beat, tempo)
+                    input_notes.append((0, delta))
+
+                #start a new note
+                last_note = msg.note
+                note_playing = True
+                last_time = 0
+
+            #note_off:
+            if (msg.type == 'note_on'  and msg.velocity == 0):
+                if last_note == msg.note:
+                    delta = tick2second(msg.time + last_time, mid.ticks_per_beat, tempo)
                     input_notes.append((msg.note, delta))
+                    note_playing = False
+                else:
+                    last_time += msg.time
             elif msg.type == 'set_tempo':
                 tempo = msg.tempo
-                print(msg)
-            elif msg.type == 'time_signature':
-                print(msg)
         if(input_notes):
             all_notes.append(input_notes)
 
@@ -118,52 +134,21 @@ class Midi_chart:
 
 
 
-def decode_midi(input, chart, has_note_off=False):
-    #n_tracks = len(input)
-    n_tracks = 1
+def decode_midi(input, chart):
+    n_tracks = len(input)
     freqs = np.array([])
     times = np.array([])
 
-    #if has_note_off:
-    if True:
-        for i in range(len(input[0])):
+    for i in range(len(input[0])):
+        if input[0][i][0] == 0:
+            note = 0
+        else:
             note = chart.notes[input[0][i][0]]
-            times = np.append(times, input[0][i][1])
-            #print("note", note) #TODO remove
-            freqs = np.append(freqs, note)
-
-    else:
-        #convert each note to freq, and time in seconds
-        print("input", input) #TODO remove
-        for i in range(len(input[0])-1, 0, -1):
-            note_tuple = input[0][i] 
-            if note_tuple[0] == 0:
-                print(note_tuple)
-                continue
-            else:
-                note = chart.notes[note_tuple[0]] #chart at index of note num is frequency
-                delta_time = note_tuple[1]
-
-                #cases: last note, not last but time=0
-                if i+1 >= len(input[0]):
-                    time = note_tuple[1]
-                    times = np.append(times, round(time, 4)) 
-                    freqs = np.append(freqs, note)
-                    #print("times", times)
-                    #print("freqs", freqs)
-                else:
-                    time = input[0][i+1][1] #the delta from the following note
-                    if time > 0:
-                        times = np.append(times, round(time, 4)) 
-                        freqs = np.append(freqs, note)
-                        #print("times", times)
-                        #print("freqs", freqs)
+        times = np.append(times, input[0][i][1])
+        freqs = np.append(freqs, note)
 
     times = np.asarray(times)
     freqs = np.asarray(freqs)
-    #if not has_note_off:
-        #freqs = np.flip(freqs)
-        #times = np.flip(times)
 
     sine_by_freq.write_from_midi(TEST, times, freqs)
 
@@ -175,9 +160,9 @@ def main():
         sys.exit(1)
     midi_file = sys.argv[1]
     print(midi_file)
-    input_notes, has_note_off = read(midi_file)
+    input_notes = read(midi_file)
     chart = Midi_chart()
-    decode_midi(input_notes, chart, has_note_off)
+    decode_midi(input_notes, chart)
 
 
 
